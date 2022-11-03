0eb39903b230ef653a5adfd0b07e665fb62ecbcc
Sai Shanmukh <msshanmukh@gmail.com>
2022-11-04 00:51:12 +0530

    urls

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
           <link rel="preload" href="/fonts/outfit-v4-latin-700.woff2" as="font" type="font/woff" crossOrigin="" />
           <link rel="preload" href="/fonts/outfit-v4-latin-regular.woff2" as="font" type="font/woff" crossOrigin="" />
           <link rel="preload" as="image" href="/blob_bg.webp" />
+          <link rel="icon" type="image/x-icon" href="/avatar.svg" />
           <meta property="og:image" content="https://shanmukh.xyz/api/og" />
 
           {process.env.NODE_ENV === 'production' && (
--- utils/Projects.ts
+++ utils/Projects.ts
@@ -1,18 +1,18 @@
 export const projects = [
   {
-    name: 'Reddit Clone',
-    descreption: 'Reddit clone has some basic reddit features which is made with bleeding edge stack ',
-    videoSrc: 'https://res.cloudinary.com/dhysgd2qs/video/upload/fl_lossy/v1665771689/screen-capture-2_koyesv.mkv',
-    tags: ['Next.js', 'Typescript', 'GraphQL', 'Apollo', 'PostgreSQL', 'Redis', 'TypeORM', 'Express', 'Docker'],
-    links: ['https://github.com/0xHecker/beddit-web', 'https://beddit-lac.vercel.app/'],
-  },
-  {
     name: 'Personal Expense Log',
     descreption:
       'Personal expense log💰 is an app to see your expense history, sorting, pagination features and help you see what you are buying lately',
     videoSrc: 'https://res.cloudinary.com/dhysgd2qs/video/upload/fl_lossy/v1666876614/screen-capture2_n9s75b.mkv',
     tags: ['React', 'Typescript', 'PostgreSQL', 'Express', 'Prisma', 'Sass'],
     links: ['https://github.com/0xHecker/personal-expense-log', 'https://personal-expense-log.vercel.app/login'],
+  },
+  {
+    name: 'Reddit Clone',
+    descreption: 'Reddit clone has some basic reddit features which is made with bleeding edge stack ',
+    videoSrc: 'https://res.cloudinary.com/dhysgd2qs/video/upload/fl_lossy/v1665771689/screen-capture-2_koyesv.mkv',
+    tags: ['Next.js', 'Typescript', 'GraphQL', 'Apollo', 'PostgreSQL', 'Redis', 'TypeORM', 'Express', 'Docker'],
+    links: ['https://github.com/0xHecker/beddit-web', 'https://beddit-lac.vercel.app/'],
   },
   {
     name: 'Fire Blog',
